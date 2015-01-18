1bad9a3ec64c8331c9377acbee1e26c80925e06b
Akshendra Pratap Singh <akshendra521994@gmail.com>
2015-01-18 15:48:34 +0530

    Worked on CSS for the results page

--- comment_box.php
+++ comment_box.php
@@ -17,6 +17,7 @@
 </div>
 
 <script>
+
 	function showComment() {
 		var commentTo = document.getElementById("comment_to").textContent.trim();
 		var data = {
--- supply_box.php
+++ supply_box.php
@@ -8,7 +8,7 @@
   if($supplies->num_rows <= 0) {
 ?>
   <div class="span9 offset1" style="color:#333; font-family:Lobster, cursive; padding: 20px 40px; border-radius: 5px;
-           box-shadow:2px 2px 10px green, -1px -1px 2px green; font-size:25px; margin-bottom:20px; text-align:center; background:#A0FFA0;">
+           box-shadow:2px 2px 5px green, -1px -1px 2px green; font-size:25px; margin-bottom:20px; text-align:center; background:#A0FFA0;">
 
   <?php echo "Well done, all clear"; ?>
   </div>
@@ -18,7 +18,7 @@
 ?>
 
   <div class="span9 offset1" style="color:#333; font-family:Lobster, cursive; padding: 20px 40px; border-radius: 5px;
-           box-shadow:2px 2px 10px red, -1px -1px 2px red; font-size:25px; margin-bottom:20px; text-align:center; background:#FFD0D0;">
+           box-shadow:2px 2px 5px red, -1px -1px 2px red; font-size:25px; margin-bottom:20px; text-align:center; background:#FFD0D0;">
 
   <?php echo "$supplies->num_rows supplies"; ?>
   </div>
