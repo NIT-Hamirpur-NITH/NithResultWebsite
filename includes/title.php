8833136286d87e5799b10cd75fbf6e9a397cc7fc
Sagar <sagarkarira1992@gmail.com>
2015-01-18 23:47:25 +0530

    Added github link in the header . Added modal

--- includes/title.php
+++ includes/title.php
@@ -3,7 +3,47 @@
 <!-- the title available on all pages -->
 <div class="hero-unit">
   <div class="section_header">
-    <h1 class="brand lead" >EXAM RESULT++ </h1>
+    <h1 class="brand lead" >NITH RESULT UPGRADED </h1>
   </div>
-  <p class="text-right muted">  -Made by <a href="http://glug.nith.ac.in/members/sagar">You</a></p>
+  <p class="text-right muted">
+  <a href="#my-modal4"  id="my-button4">
+  	<img src="./img/github_white_black_cat_32.png"></img>
+  </a>
+  <a href="https://github.com/sagarkarira" class="github-button">Follow @sagarkarira</a>
+  	<a href="https://github.com/akpakshendra" class="github-button">Follow @akpakshendra</a>
+			
+  </p>
+
 </div>
+
+
+<div class="modal hide fade" id="my-modal4">
+	<div class="modal-header">
+		<button class="close" data-dismiss="modal">&times;</button>
+		<h1 class="text-center">Its Open Source Dude </h1>
+	</div>
+	<div class="modal-body lead">
+		<p>
+
+			1.Follow the developers<br/><hr/>
+			2.Contribute to us on <br/><hr/>
+			3.Report an issue <br/><hr/>
+			4.Solve issues.<br/><hr/>
+			5.Begin now <a href ="">Click here</a>
+
+		</p>
+	</div>
+	<div class="modal-footer">
+		<a data-dismiss="modal" class="btn btn-primary ">Close</a>
+	</div>
+</div>
+<!-- Place this tag right after the last button or just before your close body tag. -->
+<script async defer id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
+<script src="js/jQuery.js"></script>
+<script src="js/bootstrap.js"></script>
+<script type="text/javascript">
+	// why this
+	$("#my-button4").click(function(){
+		$("#my-modal4").modal();
+	});
+</script>
