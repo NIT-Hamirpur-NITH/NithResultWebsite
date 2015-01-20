00ed474f2c466656254fd08609d2774058bd61c9
Sagar <sagarkarira1992@gmail.com>
2015-01-20 18:41:15 +0530

    Added star rating

--- includes/middle.php
+++ includes/middle.php
@@ -5,29 +5,26 @@
 	<img src="images/77.png" alt="Demo"  class="offset1" />
 	<h3 class="text-center text-error" >Feature Rich Result</h3>
 	<p><strong>
-		  A feature rich search by rank , roll no. and name. A classic interface.
-			Also comment and like on the result page of your friends.
+		  A feature rich search by rank , roll no. and name.Share your result , comment 
+		  on each others result page.
 	</strong></p>
 	<button class="btn   "> Read More </button>
 </div>
 
 <div class="span4 offset1">
 	<img src="images/66.png" alt="Demo"  class=" offset1" />
-	<h3 class="text-center text-error">Review this site...</h3>
+	<h3 class="text-center text-error">Review this site</h3>
 	<p ><strong>
-		If you are here then dont forget to review here, it would help me to know
-		what you people think about this. Click on the button to comment us and
-		rate us.
+		If you are here then don't' forget to rate this site. Go for the 5 stars.<br/>
 	</strong></p>
-	<button class="btn ">Review here</button>
+	<div id="14217587467165" class="wE"><script src="http://widget-engine.com/w:0:003:14217587467165:4:49afcd:40" type="text/javascript"></script></div>
+	
 </div>
 
 <div class="span4 offset1">
 	<img class="offset1" src="images/88.png" alt="Demo" />
-	<h3 class="text-center text-error">A Kind Request</h3>
+	<h3 class="text-center text-error">A Request</h3>
 	<p><strong>
-		This is a general request to please like my page on facebook which you can see at the
-		right hand side of the webpage. Also if you wish you can follow me on twitter , instagram
-		and facebook
+		Share your result on facebook . Spread the word . And please don't forget to like the facebook page you see just above . Thanks
 	</strong></p>
 </div>
